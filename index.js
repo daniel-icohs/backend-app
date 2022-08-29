b0eeb7fa4f8103f8dbd5d5105bb94478282875f0
Daniel Gomez <Daniel Gomez@iem.local>
2022-08-29 10:41:42 -0700

    backend server created

---
+++ index.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var mysql = require("mysql");
+const app = express();
+const cors = require('cors')
+
+const pool = mysql.createPool({
+    host: '173.201.183.45',
+    user: 'i5888491_wp3',
+    password: 'M.w7agIybhjmFRnDfn108',
+    database: 'i5888491_wp3'
+});
+
+pool.getConnection(function (error) {
+    if (!!error) {
+        console.log("Error")
+    } else {
+        console.log("Conected")
+    }
+
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true})) ;
+
+
+app.post("/api/icohs/insert", (req, res) => {
+
+    console.log(req.body)
+    const step1 = req.body.step1;
+    const step2 = req.body.step2;
+    const step3 = JSON.stringify(req.body.step3);
+    const step4 = req.body.step4;
+    const personalDataName = req.body.personalDataName;
+    const personalDataLastName = req.body.personalDataLastName;
+    const personalDataEmail = req.body.personalDataEmail;
+    const personalDataPhone = req.body.personalDataPhone;
+    console.log(personalDataName," PERSONAL NAME")
+
+    const sqlInsert = "INSERT INTO `icohs-quiz` (`step1`, `step2`, `step3`, `step4`, `first _name`, `last_name`, `email`, `phone`) VALUES (? , ? , ? , ?, ?, ?, ?, ?)";
+    pool.query(sqlInsert, [step1, step2, step3, step4, personalDataName, personalDataLastName, personalDataEmail, personalDataPhone], (error) => {
+        if(error) {
+            res.json({status:"failure",reason: error.code});
+        }else{
+            res.json({status:"success"})
+        }
+
+    })
+})
+
+
+app.get("/api/icohs/get", function (req, res) {
+
+    pool.query("SELECT * FROM `icohs-quiz` LIMIT 100;", function (error, rows, fieds) {
+        if (!!error) {
+            console.log("error in the query")
+            res.status(500).json(error)
+        } else {
+            console.log("SUCCES")
+            console.log(rows)
+            res.status(200).json(rows);
+        }
+    })
+});
+
+
+app.get("/api/icohs/delete", function (req, res) {
+
+    pool.query("DELETE FROM `icohs-quiz` WHERE ID = 2", function (error, rows, fieds) {
+        if (!!error) {
+            console.log("error in the query")
+            res.status(500).json(error)
+        } else {
+            console.log("SUCCES")
+            console.log(rows)
+            res.status(200).json(rows);
+        }
+    })
+});
+
+app.get("/api/icohs/update", function (req, res) {
+
+    pool.query("UPDATE `icohs-quiz` SET `step1`= 'general IT training' ,`step2`= 'IT network specialist',`step3`='already working in my field of study',`step4`='paying for tuition' WHERE id=3", function (error, rows, fieds) {
+        if (!!error) {
+            console.log("error in the query")
+            res.status(500).json(error)
+        } else {
+            console.log("SUCCES")
+            console.log(rows)
+            res.status(200).json(rows);
+        }
+    })
+});
+
+
+app.listen(3001, () => {
+    console.log("running on port 3001")
+});
